906eaa441d3e3110393ee23cb83340bfe0a99dff
Marcell-Puskas <puskasmarcell05@gmail.com>
2021-07-10 22:18:22 +0200

    Added image conversion and a screen printer

---
+++ aBox/level_maker.py
@@ -0,0 +1,67 @@
+from PIL import Image #import skimage
+import typing
+#from dataclasses import dataclass
+from rich import print
+from perams import objects
+from typing import Tuple
+
+def make_list_map(img):
+    levelimg = Image.open(img)
+    #creates a list of list of dictionaries of the map image
+    map = []
+    for y in range(0, levelimg.height):
+        map.insert(y, [])
+        for x in range(0, levelimg.width):
+            cordinate = (x, y)
+            pixel = r, g, b = levelimg.getpixel(cordinate)
+            for curent_object in objects:
+                if(pixel[0] == curent_object['r'] and pixel[1] == curent_object['g'] and pixel[2] == curent_object['b']):
+                    map[y].insert(0, {"ob":curent_object['ob']})
+
+    return map
+
+def make_string_map(img):
+    levelimg = Image.open(img)
+    #creates a string of the map image
+    mapstring = ""
+    for y in range(0, levelimg.height):
+        for x in range(0, levelimg.width):
+            cordinate = (x, y)
+            pixel = r, g, b = levelimg.getpixel(cordinate)
+            for i in range(len(objects)):
+                if(pixel[0] == objects[i]['r'] and pixel[1] == objects[i]['g'] and pixel[2] == objects[i]['b']):
+                    mapstring += objects[i]['char']
+        mapstring += "\n"
+
+    return mapstring
+
+def make_tuple_map(img):
+    levelimg = Image.open(img) 
+    #crates tupels of the cordinets of the objects
+    from dataclasses import dataclass
+    Point = tuple[int, int]
+    @dataclass
+    class LevelState:
+        img_dim: Tuple[int, int]
+        player: Point
+        wall: set[Point]
+        box: set[Point]
+    initial = LevelState(
+        img_dim=(levelimg.size),
+        player=(),
+        wall=set(),
+        box=set()
+    )
+    for y in range(0, levelimg.height):
+        for x in range(0, levelimg.width):
+            cordinate = (x, y)
+            pixel = r, g, b = levelimg.getpixel(cordinate)
+            for i in range(len(objects)):
+                if(pixel[0] == objects[i]['r'] and pixel[1] == objects[i]['g'] and pixel[2] == objects[i]['b']):
+                    if objects[i]['ob'] in ( "air","enemy"):
+                        continue
+                    if objects[i]['ob'] in ( "player"):
+                        initial.player = (x, y)
+                        continue
+                    exec(f"initial.{objects[i]['ob']}.add({cordinate})")
+    return initial
---
+++ aBox/screen.py
@@ -0,0 +1,48 @@
+from typing import Tuple
+from dataclasses import dataclass
+from perams import objects
+# from rich import print
+from blessed import Terminal
+
+from level_maker import make_tuple_map
+
+Point = tuple[int, int]
+
+term = Terminal()
+
+@dataclass
+class LevelState:
+    img_dim: Tuple[int, int]
+    player: Point
+    wall: set[Point]
+    box: set[Point]
+
+def map_print(map) -> str:
+    Map = [[" " for i in range(map.img_dim[1])] for i in range(map.img_dim[0])]
+    for i in map.wall:
+        Map[i[0]][i[1]] = "█"
+    for i in map.box:
+        Map[i[0]][i[1]] = "■"
+    Map[map.player[0]][map.player[1]] = "O"
+
+    # for line in Map:
+    #     print("".join(line))
+
+    with term.hidden_cursor():
+        print(term.home + term.clear)
+
+        for y in range(map.img_dim[1]):
+            for x in range(map.img_dim[0]):
+                if (x, y) in map.wall:
+                    print(term.move_xy(x, y) + term.white("█"))
+
+                if (x, y) in map.box:
+                    print(term.move_xy(x, y) + term.white("■"))
+
+        print(term.move_xy(map.player[0], map.player[1]) + term.white("O"))
+
+    print(term.move_y(map.img_dim[1]))
+    return Map
+
+if __name__ == "__main__":
+    map_print(make_tuple_map("level2.png"))
