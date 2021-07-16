8343d2b21aa87a61de54162d356cea0f5b627700
dain-xyz <>
2021-07-16 18:03:20 +0100

    remove unneeded parts of __main__ and make level refresh after code is run

--- aBox/__main__.py
+++ aBox/__main__.py
@@ -12,15 +12,15 @@ import time
 term = Terminal()
 
 current_level = Path("levels/level2.png")
-level = LevelState.from_image(current_level)
+# level = LevelState.from_image(current_level)
 
-functions = {
-    "move": level.move_player,
-    "==": lambda args: args[0]==args[1],
-    "+": lambda args: args[0]+args[1],
-    "grab": level.grab,
-    "ungrab": level.ungrab,
-}
+# functions = {
+#     "move": level.move_player,
+#     "==": lambda args: args[0]==args[1],
+#     "+": lambda args: args[0]+args[1],
+#     "grab": level.grab,
+#     "ungrab": level.ungrab,
+# }
 
 
 display_list = [""]
@@ -31,15 +31,17 @@ mode = prev_mode = 0 # 0 is edit, 1 is run
 
 if __name__ == '__main__':
     with term.fullscreen(), term.hidden_cursor(), term.cbreak():
-        level_print(term, level)
         while True:
+            level = LevelState.from_image(current_level)
+            level_print(term, level)
+
             key = term.inkey()
             #print(term.home + term.clear)
             #level_print(term, level)
+
             display_list, cx, cy, mode = create_box(level.size.x + 10, term, key, display_list, cx, cy)
 
             if prev_mode != mode and mode == 1: # mode changed to run mode
-                # eval(parse("".join(display_list)), functions)
                 parsed = parse("".join(display_list))
                 program = interpret(parsed)
                 
@@ -47,7 +49,7 @@ if __name__ == '__main__':
                     if isinstance(action, Action):
                         level.update(action)
                         level_print(term, level)
-                        time.sleep(1)
+                        time.sleep(0.5)
 
                 mode = 0
                 level_print(term, level)
